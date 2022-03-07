fa987594909dc3de3606b6893eb8cca95fdd9500
John Powell <powell.john2011@gmail.com>
2022-03-07 16:31:21 +0300

    adding the function to get use input in  JS file

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ let month = "";
 let day = "";
 let dayOfTheWeek= "";
 let akanName = ""
+
+const getUserInput = function (event) {
+    event.preventDefault();
+    let dateOfBirth = document.getElementById("birth-date").value;
+    let dateDetails = dateOfBirth.split('-')
+    century = parseInt(dateDetails[0].slice(0,2));
+    year = parseInt(dateDetails[0].slice(2,4));
+    month = parseInt(dateDetails[1]);
+    day = parseInt(dateDetails[2]);
+    dayOfTheWeek = Math.floor(calculateDayOfTheWeek(century,year,month,day).toFixed())
+    
+}
