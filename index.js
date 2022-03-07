5fdbd4f5ff64c30f5c512be377ffe0dab29a4cc8
John Powell <powell.john2011@gmail.com>
2022-03-07 16:28:50 +0300

    adding all the variables needed in the JS file

---
+++ index.js
@@ -0,0 +1,10 @@
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi", "Kwame"];
+const daysOfTheWeek =["Sunday","Monday" ,"Tuesday","Wednesday ","Thursday ","Friday","Saturday" ];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+let century = "";
+let year = "";
+let month = "";
+let day = "";
+let dayOfTheWeek= "";
+let akanName = ""
